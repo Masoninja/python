d0db7606c48d9d9b9e709f17d2b88048192043b8
Mason Allen <42976038+Masoninja@users.noreply.github.com>
2018-09-13 09:38:42 -0700

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,18 @@
+#loop.py
+gcat = 1
+while (gcat < 51535):
+		print(gcat,end='', flush=True)
+		print(" G",end='  ', flush=True)
+		print(gcat,end='', flush=True)
+		print(" C",end='  ', flush=True)
+		print(gcat,end='', flush=True)
+		print(" A",end='  ', flush=True)
+		print(gcat,end='', flush=True)
+		print(" T",end='  ', flush=True)
+		if( gcat != 51523) and ( gcat != 51413) == 1:
+			print(" FAILED ")
+		if( gcat == 51523) or (gcat == 51413) == 1:
+			print( " Error: Unexpeced Variable - Outcome Undetermined")
+		gcat = gcat + 1
+		
+
