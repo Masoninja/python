15f45436343fe77191c01c60916fa66557eb4d11
Mason Allen <42976038+Masoninja@users.noreply.github.com>
2018-10-02 10:21:13 -0700

    Add files via upload

---
+++ ops1.py
@@ -0,0 +1,18 @@
+#ops1.py mka
+"""
++   Addition          x + y
+-   Subtraction       x - y
+*   Multiplication    x * y  
+/   Divistion         x / y
+%   Modulus           x % y
+**  Exponentiation    x ** y
+//  Floor division    x // y
+"""
+print ("33 + 17", 33 + 17)
+print ("33 - 17", 33 + 17)
+print ("33 * 17", 33 * 17)
+print ("33 / 17", 33 / 17)
+print ("33 % 17", 33 % 17)
+print ("33 ** 17", 33 ** 17)
+print ("37 // 17", 33 // 17)
+print ("37.0 // 17.0", 37.0 // 17.0)
