7031adad619435073d32dc71b081f8604685e592
Mason Allen <42976038+Masoninja@users.noreply.github.com>
2018-10-08 10:12:18 -0700

    Add files via upload

---
+++ binarystring.py
@@ -0,0 +1,22 @@
+#binarystring.py mka
+#only works in python right now, not python3 for some reason?
+def bincon(decimal):
+	print("BINARY\n")
+	print(decimal)
+	binstr=""
+	for i in range(8):
+		bin = decimal % 2
+		binstr = binstr + str(bin)
+		decimal = decimal // 2
+		print (bin)
+	print(binstr)
+	
+def main():
+	print("INPUT A -1 TO EXIT THE LOOP ")
+	print("INPUT AN INTEGER LESS THAN 256 AND GREATER THAN -1")
+	done = 0;
+	while (done >= 0):
+		dec = input("INPUT AN INTEGER: ")
+		bincon(dec)
+		
+main()
