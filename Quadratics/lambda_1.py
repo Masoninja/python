0e67423602008771cc1289fda6b7f48688304e87
Mason Allen <42976038+Masoninja@users.noreply.github.com>
2019-01-14 10:22:41 -0800

    Add files via upload

---
+++ Quadratics/lambda_1.py
@@ -0,0 +1,5 @@
+#lambda1 mka
+
+x = lambda a, b, c : a + b + c
+fx = x(5, 6, 2)
+print(fx)
---
+++ Quadratics/lambda_3.py
@@ -0,0 +1,17 @@
+#lambda_3 mka
+
+def quadratic_function(a,b,c):
+	return lambda x: a*(x**2) + b*x + c
+	
+	
+a1 = quadratic_function(1,2,1)
+print ("a1 ",a1(1))
+print("Input a, b, and c from a quadratic equation in the form of ")
+print ("x = ax^2 + bx + c")
+a = int(input("Input a: "))
+b = int(input("Input b: "))
+c = int(input("Input c: "))
+x = int(input("Input x: "))
+a2 = quadratic_function(a,b,c)
+fx = a2(x)
+print("f(x) = ",fx)
